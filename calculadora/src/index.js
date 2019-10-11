8dfcc909065d69c972b692e8b8213fd7196212f2
Cynara Peixoto <cynara.peixoto@serpro.gov.br>
2019-10-11 11:09:02 -0300

    Calculadora

---
+++ calculadora/src/index.js
@@ -0,0 +1,16 @@
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Calculator from './main/Calculator';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <div>
+    <h1>Calculadora</h1>
+    <Calculator />
+  </div>
+  , document.getElementById('root'));
+
+serviceWorker.unregister();
